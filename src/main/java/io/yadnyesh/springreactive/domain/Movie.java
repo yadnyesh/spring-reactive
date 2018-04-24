f76f2a9b29dfff6b7e4363fce3117b00682e70f9
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-04-24 07:07:37 +0530

    Created Movie class

--- src/main/java/io/yadnyesh/springreactive/domain/Movie.java
+++ src/main/java/io/yadnyesh/springreactive/domain/Movie.java
@@ -1,5 +1,17 @@
 package io.yadnyesh.springreactive.domain;
 
-public class Movie {
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+
+@Document
+@Data
+@NoArgsConstructor
+public class Movie {
+	private String id;
+	
+	@NonNull
+	private String title;
 }
