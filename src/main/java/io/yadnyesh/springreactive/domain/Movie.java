a03bef6036a6adf177e71cae6294d6c8bd13c515
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-04-24 07:14:37 +0530

    Added Repository Interface

--- src/main/java/io/yadnyesh/springreactive/domain/Movie.java
+++ src/main/java/io/yadnyesh/springreactive/domain/Movie.java
@@ -1,5 +1,6 @@
 package io.yadnyesh.springreactive.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -9,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
 	private String id;
 	
